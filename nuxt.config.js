ad24b3b0c7476586ceeedebff7cac140b48fca25
Thomas Moellers <thomas.moellers@rwth-aachen.de>
2021-01-15 21:58:30 +0800

    test deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,8 @@
 export default {
   target: 'static',
-  // router: {
-  //   base: '/<repository-name>/'
-  // },
+  router: {
+    base: '/<repository-name>/'
+  },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
