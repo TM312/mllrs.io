68267aa01391b97d8e3097c0974be3a2499691f5
tm312 <tmoellers@protonmail.ch>
2022-02-14 15:01:51 +0800

    fix: Simplify about

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,12 @@
 <template>
     <div class="mx-auto md:container px-12 lg:px-48 xl:px-75 py-48">
         <p class="text-lg text-grey-900">
-            Hi, I'm Thomas. This is my personal blog. I write about building and
-            launching web applications and other things that interest me.
+            My personal blog about programming and other interesting things.
         </p>
 
         <div class="mt-12">
             <small class="text-grey-500"
-                >This blog is built with Nuxt, FaunaDB, and Netlify.</small
+                >Built with Nuxt, FaunaDB, and Netlify.</small
             >
         </div>
     </div>
