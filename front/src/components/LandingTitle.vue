f807460d527c73adcc9b0f9cabe4bd9545d7d08d
tm312 <tmoellers@protonmail.ch>
2023-05-30 08:39:05 +0800

    text: Rewording landing

--- front/src/components/LandingTitle.vue
+++ front/src/components/LandingTitle.vue
@@ -5,17 +5,21 @@
       <div>
         <h1
           class="inline-block max-w-lg align-top mt-2 bg-gradient-to-br from-white to-gray-600 bg-clip-text text-transparent text-2xl leading-[28px] sm:text-4xl sm:leading-[48px] text-gray-400 font-bold">
-          Hi, I'm Thomas,<br /> I build digital products for
-          the web</h1>
+          <br />Building digital products for the web
+        </h1>
         <p
           class="mt-4 mb-8 text-base font-normal text-gray-400">
-          <span class="inline-block max-w-[591px] align-top">I
-            enjoy working across the full stack and on
-            NLP-related problems. Have a look at some of my
-            projects or check out my resume. If you have any
-            questions or would like to discuss a potential
-            collaboration, I'd be thrilled to connect with
-            you.</span>
+          <span
+            class="inline-block max-w-[591px] align-top">Hi,
+            I'm Thomas, I'm a software engineer and maker
+            based in Singapore. I enjoy working across the
+            full stack and on NLP-related problems. Have a
+            look at some of my projects or check out my <a
+              href="/about"
+              class="font-semibold hover:text-gray-200">resume</a>.
+            If you have any questions or would like to discuss
+            a potential collaboration, I'd be thrilled to
+            connect with you.</span>
         </p>
       </div>
     </transition>
